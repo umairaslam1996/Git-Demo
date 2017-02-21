cda22e366fad66bc6b7de70532b4170227faf2e3
umairaslam1996 <umairasalam1996@gmail.com>
2017-02-21 11:15:32 +0500

    helo

---
+++ Git demo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Git demo
+//
+//  Created by Umair Aslam on 21/02/2017.
+//  Copyright © 2017 IOS Training. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
